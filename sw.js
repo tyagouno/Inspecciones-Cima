cfdd1ad4d352d3c5f505fd61f4bc79923e3a6ab1
tyagouno <tyagouno@gmail.com>
2026-02-13 00:28:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cima-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
